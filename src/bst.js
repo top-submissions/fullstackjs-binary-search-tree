b5142151209c9e9eaa4d0d288d34de731f3be2c0
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-05 10:40:17 +0800

    feat: add Node class

---
+++ src/bst.js
@@ -0,0 +1,10 @@
+/**
+ * Node class represents a single node in the binary search tree
+ */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
