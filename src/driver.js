c358ece275f414239e885e3d3d08d667d60d3f63
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-05 15:44:06 +0800

    feat: add print all orders function

--- src/driver.js
+++ src/driver.js
@@ -10,3 +10,31 @@ import prettyPrint from './utils/prettyPrint.js';
 function generateRandomArray(size, max) {
   return Array.from({ length: size }, () => Math.floor(Math.random() * max));
 }
+
+/**
+ * Prints all elements in different traversal orders
+ * @param {Tree} tree - Binary search tree to traverse
+ */
+function printAllOrders(tree) {
+  console.log('\n📊 Traversal Orders:');
+
+  // Level Order
+  const levelOrder = [];
+  tree.levelOrderForEach((node) => levelOrder.push(node.data));
+  console.log('Level Order:   ', levelOrder.join(', '));
+
+  // Pre Order
+  const preOrder = [];
+  tree.preOrderForEach((node) => preOrder.push(node.data));
+  console.log('Pre Order:     ', preOrder.join(', '));
+
+  // In Order
+  const inOrder = [];
+  tree.inOrderForEach((node) => inOrder.push(node.data));
+  console.log('In Order:      ', inOrder.join(', '));
+
+  // Post Order
+  const postOrder = [];
+  tree.postOrderForEach((node) => postOrder.push(node.data));
+  console.log('Post Order:    ', postOrder.join(', '));
+}
